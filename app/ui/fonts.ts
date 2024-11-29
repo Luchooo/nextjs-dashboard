1145902bc48a0e86691f2f0ba236b60d09ec5e5e
Luis Sanchez <luis.udec.sanchez@gmail.com>
2024-11-28 20:52:43 -0500

    [fix] Remove fonts unnecesaries

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,8 +1,6 @@
-import { Alexandria, Bokor, Inter, Lusitana } from "next/font/google";
+import { Inter, Lusitana } from "next/font/google";
 
 export const inter = Inter({ subsets: ["latin"] });
-export const alexandria = Alexandria({ subsets: ["latin"] });
-export const bokor = Bokor({ weight: "400" });
 export const lusitana = Lusitana({
 	weight: ["400", "700"],
 	subsets: ["latin"],
